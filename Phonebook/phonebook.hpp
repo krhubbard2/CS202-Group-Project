4ea2d7b63cdbef3277b5524b8f8e23e977344f0b
Kelby <krhubbard2@alaska.edu>
2020-02-20 09:17:54 -0900

    Added prototypes to phonebook.hpp -- has outline of program

--- Phonebook/phonebook.hpp
+++ Phonebook/phonebook.hpp
@@ -38,4 +38,50 @@ using std::find_if;
 
 
 
+
+//////////////////////////////////////////////////////
+// P R O T O T Y P E S ///////////////////////////////
+//////////////////////////////////////////////////////
+
+//Displays main menu
+void printMenu();
+
+//Adds a new record to vector
+void addRecord(vector<tuple<string, string, double>> &vec);
+
+//Sorts all current records by last name
+bool sortVec(const tuple<string, string, double>&a,
+          const tuple<string, string, double>&b);
+
+//Prints all current records
+void printRecords(vector<tuple<string, string, double>> &vec);
+
+//Allows user to modify existing records.
+void modifyRecord(vector<tuple<string, string, double>> &vec);
+
+//Search records by last name
+void searchRecordLast(vector<tuple<string, string, double>> vec, string name);
+
+//Search records by first name
+void searchRecordFirst(vector<tuple<string, string, double>> vec, string name);
+
+//Search records by phone number
+void searchRecordPhone(vector<tuple<string, string, double>> vec, double phone);
+
+//Delete record by last name
+void deleteRecordLast(vector<tuple<string, string, double>> &vec, string name);
+
+//Delete record by first name
+void deleteRecordFirst(vector<tuple<string, string, double>> &vec, string name);
+
+//Delete record by phone number
+void deleteRecordPhone(vector<tuple<string, string, double>> &vec, double phone);
+
+//Searches existing records
+void searchRecord(vector<tuple<string, string, double>> &vec);
+
+//Deletes record (implaments deleteRecord(F,L,P))
+void deleteRecord(vector<tuple<string, string, double>> &vec);
+
+
 #endif
