7364cef5aac8cba2d5c819c086355bf5ea89957a
Kelby <krhubbard2@alaska.edu>
2020-02-20 09:17:28 -0900

    Added Libarries to phonebook.hpp

---
+++ Phonebook/phonebook.hpp
@@ -0,0 +1,41 @@
+// Kelby Hubbard
+// Chaluunbat Purev
+// CS202 Group Project
+// The Alaskan Phonebook
+#ifndef PHONEBOOK_HPP_
+#define PHONEBOOK_HPP_
+
+//////////////////////////////////////////////////////
+// L I B R A R I E S /////////////////////////////////
+//////////////////////////////////////////////////////
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#include <string>
+using std::string;
+using std::getline;
+#include <fstream>
+using std::ofstream;
+using std::ifstream;
+#include <sstream>
+using std::istringstream;
+#include <vector>
+using std::vector;
+#include <tuple>
+using std::tuple;
+using std::make_tuple;
+using std::get;
+#include <iomanip>
+using std::setw;
+using std::fixed;
+using std::setprecision;
+#include <algorithm>
+using std::sort;
+using std::find;
+using std::find_if;
+
+
+
+#endif
