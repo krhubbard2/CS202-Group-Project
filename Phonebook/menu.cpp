b7bacf6e235df1fde1f6cca837c4156ccc1002db
Kelby <krhubbard2@alaska.edu>
2020-03-07 16:45:54 -0900

    Implemented printMenu() in menu.cpp

--- Phonebook/menu.cpp
+++ Phonebook/menu.cpp
@@ -3,3 +3,13 @@
 // CS202 Group Project
 // The Alaskan Phonebook
 #include "phonebook.hpp"
+//Prints Main Menu
+void printMenu()
+{
+  cout << "1) Add telephone record" << endl;
+  cout << "2) Show records" << endl;
+  cout << "3) Modify record" << endl;
+  cout << "4) Search telephone record" << endl;
+  cout << "5) Delete record" << endl;
+  cout << "6) Exit" << endl;
+}
