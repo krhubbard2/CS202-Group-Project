2fb59cc5618d92f4eb6c464a7f5c81216404689b
Kelby <krhubbard2@alaska.edu>
2020-03-07 16:46:17 -0900

    Implemented addRecord to menu.cpp

--- Phonebook/menu.cpp
+++ Phonebook/menu.cpp
@@ -13,3 +13,59 @@ void printMenu()
   cout << "5) Delete record" << endl;
   cout << "6) Exit" << endl;
 }
+
+
+//Adds record to existing Vector
+void addRecord(vector<tuple<string, string, double>> &vec)
+{
+  cout << "Adding new record." << endl;
+  cout << "First name: ";
+
+  //Grabs First Name
+  string firstname;
+  getline(cin, firstname);
+
+  cout << "Last name: ";
+
+  //Grabs Last Name
+  string lastname;
+  getline(cin, lastname);
+
+  double phone;
+
+  //Grabs Phone Number (Ensures proper input)
+  bool loop1 = true;
+  while (loop1)
+  {
+    cout << "Phone number including area code (no \"-\" or \".\"): ";
+    string number;
+    getline (cin, number);
+    istringstream iss1(number);
+    double num;
+    iss1 >> num;
+
+    //Ensures phone number is a double
+    if (!iss1)
+    {
+      cout << "Error. Improper entry. Please enter a 9 digit phone number." << endl;
+      loop1 = true;
+    }
+    else
+    {
+      //Ensure number is proper area code and length
+      if (num <= 2000000000 || num >= 9999999999)
+      {
+        cout << "Error. Invalid phone number." << endl;
+        loop1 = true;
+      }
+      else
+      {
+        phone = num;
+        loop1 = false;
+      }
+    }
+  }
+  //Adds input to Vector
+  vec.push_back(make_tuple(firstname, lastname, phone));
+  cout << "Entry added succesfully." << endl;
+}
