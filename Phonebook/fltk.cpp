bd733c12cc716f019c2c425d5f4630dc763d62fa
Kelby <krhubbard2@alaska.edu>
2020-03-29 11:47:05 -0800

    Write FLTK quitButton

--- Phonebook/fltk.cpp
+++ Phonebook/fltk.cpp
@@ -20,27 +20,34 @@ void open(Fl_Widget* w, void* data)
   cout << "open" << endl;
 }
 
+void yesQuit(Fl_Widget* w, void* data)
+{
+  exit(0);
+}
+
 //Quit function for menu dropdown bar
 void quit(Fl_Widget* w, void* data)
 {
-  cout << "quit" << endl;
+   Fl_Window* window = new Fl_Window(340, 150, "Quit");
+   Fl_Box* box = new Fl_Box(20, 40, 300, 35, "Are you sure you want to quit?");
+   Fl_Button* yesButton = new Fl_Button(120, 100, 100, 25, "Yes I\'m sure.");
+   yesButton->callback(yesQuit);
+
+   box->show();
+   window->show();
+  // exit(0);
 }
 
+//Main run function for FLTK/GUI
 Fl_Double_Window* runFLTK() {
-
-
 //Main Window
   Fl_Double_Window* w;
   { Fl_Double_Window* window = new Fl_Double_Window
                                               (605, 405, "Alaskan Phonebook");
     w = window;
 
-    // { Fl_Menu_Button* menuButton = new Fl_Menu_Button(0, 0, 70, 25, "Menu");
-    // }
-
     //Drop down menu bar
     { Fl_Menu_Bar* menuBar = new Fl_Menu_Bar(0, 0, 75, 25);
-
       //When selected runs "open()"" in fltk.cpp
       menuBar->add("File/Open", 0, open);
       //When selected runs "save()"" in fltk.cpp
@@ -50,13 +57,11 @@ Fl_Double_Window* runFLTK() {
     }
 
 
-  //  { new Fl_Button(0, 25, 145, 25, "Save"); }
-
     window->end();
     window->resizable(window);
 
     window->show();
-  } // Fl_Double_Window* o
+  }
 
 
 
